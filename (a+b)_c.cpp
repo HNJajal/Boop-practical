d61a8a8c1904399261112d2152ce7f573513fec6
HNJajal <162157643+HNJajal@users.noreply.github.com>
2024-03-21 20:44:17 +0530

    Add files via upload

---
+++ (a+b)_c.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,an,ans;
+    cout<<"Enter the value of a"<<endl;
+    cin >> a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+    cout<<"Enter the value of c"<<endl;
+    cin>>c;
+    an=a+b;
+    ans=an*c;
+    cout << "answrr is " <<ans<< endl;
+    
+    
+    
+    return 0;
+}
