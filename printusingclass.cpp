4539299a35f7837cf49b1e9f269e6e0e05fa38fc
HNJajal <162157643+HNJajal@users.noreply.github.com>
2024-04-06 12:52:55 +0530

    Add files via upload

---
+++ printusingclass.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class name{
+    public:
+    void nme();
+    };
+void name::nme(){
+    cout << "My name is Harsh" << endl;
+}
+int main()
+{
+    name n1;
+    n1.nme();
+    return 0;
+}
